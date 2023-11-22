7914dea080aff68f82cc184d869cf03b18d6431c
Ufuk Uzun <ufukuzun.ce@gmail.com>
2023-11-22 11:53:26 +0300

    preparations for java 21

---
+++ src/test/java/io/github/ufuk/java20/Java20Tests.java
@@ -0,0 +1,17 @@
+package io.github.ufuk.java20;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Further readings:
+ * - ...
+ */
+class Java20Tests {
+
+    @Test
+    void todo() {
+        Assertions.fail("No example presents"); // TODO: add example(s)
+    }
+
+}
---
+++ src/test/java/io/github/ufuk/java21/Java21Tests.java
@@ -0,0 +1,17 @@
+package io.github.ufuk.java21;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Further readings:
+ * - ...
+ */
+class Java21Tests {
+
+    @Test
+    void todo() {
+        Assertions.fail("No example presents"); // TODO: add example(s)
+    }
+
+}
