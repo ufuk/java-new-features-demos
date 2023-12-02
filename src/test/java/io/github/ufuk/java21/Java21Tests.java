9461782e2d7355a4bff5ea7f298312751eb9c69a
Ufuk Uzun <ufukuzun.ce@gmail.com>
2023-12-03 00:48:57 +0300

    java 21 examples (sequenced collections & maps)

--- src/test/java/io/github/ufuk/java21/Java21Tests.java
+++ src/test/java/io/github/ufuk/java21/Java21Tests.java
@@ -2,6 +2,14 @@ package io.github.ufuk.java21;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Further readings:
@@ -9,6 +17,107 @@ import org.junit.jupiter.api.Test;
  * - https://www.happycoders.eu/java/java-21-features/
  */
 class Java21Tests {
+
+    @ParameterizedTest
+    @MethodSource("provideSequencedCollections")
+    void sequenced_collections(SequencedCollection<Integer> sequencedCollection) {
+        // at first, contains only 1
+        assertThat(sequencedCollection).hasSize(1).contains(1);
+        assertThat(sequencedCollection.getFirst()).isEqualTo(1);
+        assertThat(sequencedCollection.getLast()).isEqualTo(1);
+
+        // add to start
+        sequencedCollection.addFirst(0);
+        // add to end
+        sequencedCollection.addLast(2);
+        assertThat(sequencedCollection).hasSize(3).containsSequence(0, 1, 2);
+
+        // get first item
+        assertThat(sequencedCollection.getFirst()).isEqualTo(0);
+
+        // get first item
+        assertThat(sequencedCollection.getLast()).isEqualTo(2);
+
+        // get reversed collection
+        assertThat(sequencedCollection.reversed()).hasSize(3).containsSequence(2, 1, 0);
+
+        // remove from first
+        Integer first = sequencedCollection.removeFirst();
+        // remove from last
+        Integer last = sequencedCollection.removeLast();
+        assertThat(first).isEqualTo(0);
+        assertThat(last).isEqualTo(2);
+        assertThat(sequencedCollection).hasSize(1).contains(1);
+    }
+
+    static Stream<Arguments> provideSequencedCollections() {
+        return Stream.of(
+                Arguments.of(new ArrayList<>(List.of(1))),
+                Arguments.of(new LinkedList<>(List.of(1))),
+                Arguments.of(new LinkedHashSet<>(List.of(1)))
+                /*
+                This is also SequencedCollection, but it does not support first/last item modification
+                // Arguments.of(new TreeSet<>(List.of(1)))
+                 */
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideSequencedMaps")
+    void sequenced_maps(SequencedMap<String, Integer> sequencedMap) {
+        // at first, contains only 1
+        assertThat(sequencedMap).hasSize(1).containsEntry("one", 1);
+        assertThat(sequencedMap.firstEntry().getKey()).isEqualTo("one");
+        assertThat(sequencedMap.firstEntry().getValue()).isEqualTo(1);
+        assertThat(sequencedMap.lastEntry().getKey()).isEqualTo("one");
+        assertThat(sequencedMap.lastEntry().getValue()).isEqualTo(1);
+
+        // add to start
+        sequencedMap.putFirst("zero", 0);
+        // add to end
+        sequencedMap.putLast("two", 2);
+        assertThat(sequencedMap.sequencedKeySet()).hasSize(3).containsSequence("zero", "one", "two");
+        assertThat(sequencedMap.sequencedValues()).hasSize(3).containsSequence(0, 1, 2);
+        assertThat(sequencedMap.sequencedEntrySet()).hasSize(3).containsSequence(Map.entry("zero", 0), Map.entry("one", 1), Map.entry("two", 2));
+
+        // get first entry
+        assertThat(sequencedMap.firstEntry().getKey()).isEqualTo("zero");
+        assertThat(sequencedMap.firstEntry().getValue()).isEqualTo(0);
+        assertThat(sequencedMap.sequencedKeySet().getFirst()).isEqualTo("zero");
+        assertThat(sequencedMap.sequencedValues().getFirst()).isEqualTo(0);
+
+        // get last entry
+        assertThat(sequencedMap.lastEntry().getKey()).isEqualTo("two");
+        assertThat(sequencedMap.lastEntry().getValue()).isEqualTo(2);
+        assertThat(sequencedMap.sequencedKeySet().getLast()).isEqualTo("two");
+        assertThat(sequencedMap.sequencedValues().getLast()).isEqualTo(2);
+
+        // get reversed entry
+        assertThat(sequencedMap.reversed().sequencedKeySet()).hasSize(3).containsSequence("two", "one", "zero");
+        assertThat(sequencedMap.reversed().sequencedValues()).hasSize(3).containsSequence(2, 1, 0);
+        assertThat(sequencedMap.reversed().sequencedEntrySet()).hasSize(3).containsSequence(Map.entry("two", 2), Map.entry("one", 1), Map.entry("zero", 0));
+
+        // poll from start
+        Map.Entry<String, Integer> firstEntry = sequencedMap.pollFirstEntry();
+        // poll from end
+        Map.Entry<String, Integer> lastEntry = sequencedMap.pollLastEntry();
+        assertThat(firstEntry.getKey()).isEqualTo("zero");
+        assertThat(firstEntry.getValue()).isEqualTo(0);
+        assertThat(lastEntry.getKey()).isEqualTo("two");
+        assertThat(lastEntry.getValue()).isEqualTo(2);
+        assertThat(sequencedMap.sequencedEntrySet()).hasSize(1).containsSequence(Map.entry("one", 1));
+    }
+
+    static Stream<Arguments> provideSequencedMaps() {
+        return Stream.of(
+                Arguments.of(new LinkedHashMap<>(Map.of("one", 1)))
+                /*
+                These are also SequencedMap, but they do not support first/last entry modification
+                // Arguments.of(new ConcurrentSkipListMap<>(Map.of("one", 1))),
+                // Arguments.of(new TreeMap<>(Map.of("one", 1)))
+                 */
+        );
+    }
 
     @Test
     void todo() {
