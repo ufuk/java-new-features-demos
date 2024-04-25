ccbfa6f2115d69eea91186cda1d02f4dd1933e85
Ufuk Uzun <ufukuzun.ce@gmail.com>
2024-04-25 16:07:16 +0300

    java 22 preparations

---
+++ src/test/java/io/github/ufuk/java22/Java22Tests.java
@@ -0,0 +1,17 @@
+package io.github.ufuk.java22;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Further readings:
+ * - ...
+ */
+class Java22Tests {
+
+    @Test
+    void todo() {
+        Assertions.fail("No example presents"); // TODO: add example(s)
+    }
+
+}
